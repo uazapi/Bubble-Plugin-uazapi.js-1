f16c1d878f72e54f2ded4c3c8c306c70168d53b8
uazapi <123725746+uazapi@users.noreply.github.com>
2023-08-01 10:25:56 -0300

    ok

--- elements/AAw-850qe/element_actions/ALF-8564c/run.js
+++ elements/AAw-850qe/element_actions/ALF-8564c/run.js
@@ -1,12 +1,97 @@
-function(instance, properties, context) {
+function(properties, context) {
+  let baseUrl = properties.url;
+  if (!baseUrl || baseUrl.trim() === "" || !baseUrl.includes("http")) {
+      baseUrl = context.keys["Server URL"];
+  }
 
+  if (baseUrl) {
+  baseUrl = baseUrl.trim();
+  }
+  if (baseUrl && baseUrl.endsWith("/")) {
+      baseUrl = baseUrl.slice(0, -1);
+  }
 
-  //Load any data 
+  let apikey = properties.apikey;
+  if (!apikey || apikey.trim() === "") {
+      apikey = context.keys["Global APIKEY"];
+  }
+  
+  if (apikey) {
+  apikey = apikey.trim();
+  }
+      
+  let instancia = properties.instancia;
+  if (!instancia || instancia.trim() === "") {
+      instancia = context.keys["Instancia"];
+  }
 
+      var url = baseUrl + "/message/sendStatus/" + instancia;
+      
+      let headers = {
+          "Accept": "*/*",
+          "Connection": "keep-alive",
+          "Content-Type": "application/json",
+          "apikey": apikey
+      };
+      
+      var raw =  {
+          "statusMessage" : {
+              "type": "text",
+              "content": properties.content,
+              "backgroundColor": properties.backgroundColor,
+              "font": properties.font,
+              "allContacts": true
+                }
+             };
+     
+      
+          let requestOptions = {
+              method: 'POST',
+              headers: headers,
+              body: raw,  
+              uri: url,
+              json: true
+          };
+  
+  
+      let sentRequest;
+      let error;
+      error = false;
+      let error_log;
 
-
-  //Do the operation
-
-
-
-}
+      try {
+          sentRequest = context.request(requestOptions);
+     } catch(e) {
+          error = true;
+          error_log = e.toString();
+      }
+  
+      if (sentRequest.statusCode.toString().charAt(0) !== "2") {
+          error = true;
+         
+          return {
+              error: error,
+              error_log: JSON.stringify(sentRequest.body, null, 2).replace(/"_p_/g, "\""),
+          }
+      } 
+  
+      let resultObj;
+      try {
+          resultObj = sentRequest.body;
+     } catch(e) {
+          error = true;
+          error_log = `Error getting response body: ${e.toString()}`;
+      }
+  
+    
+          return {
+              error: error,
+              log: JSON.stringify(resultObj, null, 2).replace(/"_p_/g, "\""),
+              error_log: error_log,
+          };
+  
+  
+  
+  
+  
+  }
