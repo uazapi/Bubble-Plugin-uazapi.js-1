fc2b748b58f199c7fcf5d3fc39781cb51b2c4268
uazapi <123725746+uazapi@users.noreply.github.com>
2023-08-01 08:59:57 -0300

    ok

--- elements/AAw-850qe/element_actions/AKV-855nc/run.js
+++ elements/AAw-850qe/element_actions/AKV-855nc/run.js
@@ -1,12 +1,102 @@
-function(instance, properties, context) {
+function(properties, context) {
+  let baseUrl = properties.url;
+  if (!baseUrl || baseUrl.trim() === "" || !baseUrl.includes("http")) {
+      baseUrl = context.keys["Server URL"];
+  }
 
+  if (baseUrl) {
+  baseUrl = baseUrl.trim();
+  }
+  if (baseUrl && baseUrl.endsWith("/")) {
+      baseUrl = baseUrl.slice(0, -1);
+  }
 
-  //Load any data 
+  let apikey = properties.apikey;
+  if (!apikey || apikey.trim() === "") {
+      apikey = context.keys["Global APIKEY"];
+  }
+  
+  if (apikey) {
+  apikey = apikey.trim();
+  }
+      
+  let instancia = properties.instancia;
+  if (!instancia || instancia.trim() === "") {
+      instancia = context.keys["Instancia"];
+  }
 
+      var url = baseUrl + "/message/sendWhatsAppAudio/" + instancia;
+      
+      let headers = {
+          "Accept": "*/*",
+          "Connection": "keep-alive",
+          "Content-Type": "application/json",
+          "apikey": apikey
+      };
+      
+      var raw =  {
+    "number": properties.number,
+    "stickerMessage": {
+      "image": properties.image
+        },
+    "options": {
+      "delay": properties.delay,
+      "presence": "composing"
+       }
+      };
+     
+      
+          let requestOptions = {
+              method: 'POST',
+              headers: headers,
+              body: raw,  
+              uri: url,
+              json: true
+          };
+  
+  
+      let sentRequest;
+      let error;
+      error = false;
+      let error_log;
 
-
-  //Do the operation
-
-
-
-}
+      try {
+          sentRequest = context.request(requestOptions);
+     } catch(e) {
+          error = true;
+          error_log = e.toString();
+      }
+  
+      if (sentRequest.statusCode.toString().charAt(0) !== "2") {
+          error = true;
+         
+          return {
+              error: error,
+              error_log: JSON.stringify(sentRequest.body, null, 2).replace(/"_p_/g, "\""),
+          }
+      } 
+  
+      let resultObj;
+      try {
+          resultObj = sentRequest.body;
+     } catch(e) {
+          error = true;
+          error_log = `Error getting response body: ${e.toString()}`;
+      }
+  
+    
+          return {
+              remoteJid: resultObj?.key?.remoteJid,
+              fromMe: resultObj?.key?.fromMe,
+              id: resultObj?.key?.id,
+              status: resultObj?.status ? resultObj?.status.toString() : undefined,
+              error: error,
+              log: JSON.stringify(resultObj, null, 2).replace(/"_p_/g, "\""),
+              error_log: error_log,
+          };
+  
+  
+  
+  
+  
+  }
