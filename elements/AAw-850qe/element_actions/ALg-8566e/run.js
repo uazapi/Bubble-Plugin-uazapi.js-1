a31b33dfee751e4aa0919fac9b2c287f28cb0599
uazapi <123725746+uazapi@users.noreply.github.com>
2023-08-01 14:20:18 -0300

    Revert "Automatic commit from Bubble"
    This reverts commit 22cc3723a3ad338bbf15f2465d6d738c916b26da.

--- elements/AAw-850qe/element_actions/ALg-8566e/run.js
+++ elements/AAw-850qe/element_actions/ALg-8566e/run.js
@@ -1,11 +1,86 @@
 function(instance, properties, context) {
+    
+  let baseUrl = properties.url;
+  if (!baseUrl || baseUrl.trim() === "" || !baseUrl.includes("http")) {
+      baseUrl = context.keys["Server URL"];
+  }
+
+      if (baseUrl) {
+    baseUrl = baseUrl.trim();
+    }
+  if (baseUrl && baseUrl.endsWith("/")) {
+      baseUrl = baseUrl.slice(0, -1);
+  }
+
+    let instancia = properties.instancia;
+    if (!instancia || instancia.trim() === "") {
+        instancia = context.keys["Instancia"];
+    }
+
+    var url = baseUrl + "/chat/updateProfileStatus/" + instancia;
+  
+  
+  
+  var myHeaders = new Headers();
+  myHeaders.append("Accept", "*/*");
+  myHeaders.append("Connection", "keep-alive");
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("apikey", properties.apikey);
+  
+
+  var raw = JSON.stringify(
+    {
+      "status": properties.status
+    }
+  );
+
+  
+
+  var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+     
+  };
+  
 
 
-  //Load any data 
+instance.publishState('resultado', '');
+instance.publishState('error', false);
+instance.publishState('error_log', '');
+
+fetch(url, requestOptions)
+.then(response => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
+})
+.then(resultObj => {
+  
+
+  if (Object.keys(resultObj).length > 0) {
+ 
+    instance.publishState('resultado', JSON.stringify(resultObj, null, 2).replace(/"_p_/g, "\""));
+    
+  }
+})
+.catch(error => {
+  instance.publishState('error', true);
+try {
+        // Tentativa de converter a mensagem de erro para um objeto e formatá-lo como JSON
+        let errorString = error.toString().replace(/"_p_/g, "\"");
+        let errorObject = JSON.parse(errorString);
+        let formattedError = JSON.stringify(errorObject, null, 2);
+        instance.publishState('error_log', formattedError);
+       } catch(e) {
+        // Se a conversão falhar, apenas use a mensagem de erro como uma string
+        let errorString = error.toString().replace(/"_p_/g, "\"");
+        instance.publishState('error_log', errorString);
+        }
 
 
-
-  //Do the operation
+});
 
 
 
