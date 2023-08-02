4634c4d08901da7f237b87ba4ebd401c7d28fb13
uazapi <123725746+uazapi@users.noreply.github.com>
2023-08-02 19:22:20 -0300

    ajuste

--- elements/AAw-850qe/element_actions/AKv-855qc/run.js
+++ elements/AAw-850qe/element_actions/AKv-855qc/run.js
@@ -1,97 +1,97 @@
-function(properties, context) {
-  let baseUrl = properties.url;
-  if (!baseUrl || baseUrl.trim() === "" || !baseUrl.includes("http")) {
-      baseUrl = context.keys["Server URL"];
-  }
-
-  if (baseUrl) {
-  baseUrl = baseUrl.trim();
-  }
-  if (baseUrl && baseUrl.endsWith("/")) {
-      baseUrl = baseUrl.slice(0, -1);
-  }
-
-  let apikey = properties.apikey;
-  if (!apikey || apikey.trim() === "") {
-      apikey = context.keys["Global APIKEY"];
-  }
-  
-  if (apikey) {
-  apikey = apikey.trim();
-  }
-      
-  let instancia = properties.instancia;
-  if (!instancia || instancia.trim() === "") {
-      instancia = context.keys["Instancia"];
-  }
-
-      var url = baseUrl + "/message/sendStatus/" + instancia;
-      
-      let headers = {
-          "Accept": "*/*",
-          "Connection": "keep-alive",
-          "Content-Type": "application/json",
-          "apikey": apikey
-      };
-      
-      var raw =  {
-          "statusMessage" : {
-              "type": "text",
-              "content": properties.content,
-              "backgroundColor": properties.backgroundColor,
-              "font": properties.font,
-              "allContacts": true
-                }
-             };
-     
-      
-          let requestOptions = {
-              method: 'POST',
-              headers: headers,
-              body: raw,  
-              uri: url,
-              json: true
-          };
-  
-  
-      let sentRequest;
-      let error;
-      error = false;
-      let error_log;
-
-      try {
-          sentRequest = context.request(requestOptions);
-     } catch(e) {
-          error = true;
-          error_log = e.toString();
-      }
-  
-      if (sentRequest.statusCode.toString().charAt(0) !== "2") {
-          error = true;
-         
-          return {
-              error: error,
-              error_log: JSON.stringify(sentRequest.body, null, 2).replace(/"_p_/g, "\""),
-          }
-      } 
-  
-      let resultObj;
-      try {
-          resultObj = sentRequest.body;
-     } catch(e) {
-          error = true;
-          error_log = `Error getting response body: ${e.toString()}`;
-      }
-  
+  function(instance, properties, context) {
     
-          return {
-              error: error,
-              log: JSON.stringify(resultObj, null, 2).replace(/"_p_/g, "\""),
-              error_log: error_log,
-          };
-  
-  
-  
-  
-  
-  }
+    let baseUrl = properties.url;
+    if (!baseUrl || baseUrl.trim() === "" || !baseUrl.includes("http")) {
+        baseUrl = context.keys["Server URL"];
+    }
+
+    if (baseUrl) {
+    baseUrl = baseUrl.trim();
+    }
+    if (baseUrl && baseUrl.endsWith("/")) {
+        baseUrl = baseUrl.slice(0, -1);
+    }
+
+
+    let instancia = properties.instancia;
+    if (!instancia || instancia.trim() === "") {
+        instancia = context.keys["Instancia"];
+    }
+
+    var url = baseUrl + "/message/sendStatus/" + instancia;
+    
+     
+    
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "*/*");
+    myHeaders.append("Connection", "keep-alive");
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("uazapi", "true");
+    myHeaders.append("apikey", properties.apikey);
+    
+
+    var raw =  {
+        "statusMessage" : {
+            "type": "text",
+            "content": properties.content,
+            "backgroundColor": properties.backgroundColor,
+            "font": properties.font,
+            "allContacts": true
+              }
+           };
+
+    
+
+    // Converta o objeto raw em uma string JSON
+    var rawString = JSON.stringify(raw);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: rawString,
+       
+    };
+    
+
+
+instance.publishState('resultado', '');
+instance.publishState('error', false);
+instance.publishState('error_log', '');
+instance.publishState('lastmsg', '');
+
+fetch(url, requestOptions)
+    .then(response => {
+        if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(resultObj => {
+       
+               
+        if (Object.keys(resultObj).length > 0) {
+            instance.publishState('resultado', JSON.stringify(resultObj, null, 2).replace(/"_p_/g, "\""));
+      
+            
+            instance.publishState('lastmsg', resultObj);
+            
+        }
+    })
+    .catch(error => {
+        instance.publishState('error', true);
+        try {
+        // Tentativa de converter a mensagem de erro para um objeto e formatá-lo como JSON
+        let errorString = error.toString().replace(/"_p_/g, "\"");
+        let errorObject = JSON.parse(errorString);
+        let formattedError = JSON.stringify(errorObject, null, 2);
+        instance.publishState('error_log', formattedError);
+           
+       } catch(e) {
+        // Se a conversão falhar, apenas use a mensagem de erro como uma string
+        let errorString = error.toString().replace(/"_p_/g, "\"");
+        instance.publishState('error_log', errorString);
+        }
+    });
+
+
+}
