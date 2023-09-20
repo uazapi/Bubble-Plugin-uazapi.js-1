482ad5ab3ced947c32ce97a7e8467a9d44af3d27
uazapi <123725746+uazapi@users.noreply.github.com>
2023-09-20 13:34:55 -0300

    fundindo

---
+++ elements/AAC-850m6/element_actions/ABe-8516a/run.js
@@ -0,0 +1,110 @@
+function(instance, properties, context) {
+    
+  let baseUrl = properties.url;
+  if (!baseUrl || baseUrl.trim() === "" || !baseUrl.includes("http")) {
+      baseUrl = context.keys["Server URL"];
+  }
+
+  if (baseUrl) {
+  baseUrl = baseUrl.trim();
+  }
+  if (baseUrl && baseUrl.endsWith("/")) {
+      baseUrl = baseUrl.slice(0, -1);
+  }
+
+
+  let instancia = properties.instancia;
+  if (!instancia || instancia.trim() === "") {
+      instancia = context.keys["Instancia"];
+  }
+
+  var url = baseUrl + "/automate/fetchConversation/" + instancia;
+  
+  
+  
+  var myHeaders = new Headers();
+  myHeaders.append("Accept", "*/*");
+  myHeaders.append("Connection", "keep-alive");
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("uazapi", "true");
+  myHeaders.append("apikey", properties.apikey);
+  
+
+  var raw = {};
+
+  
+  if (properties.name && properties.name.trim() !== "") {
+    raw["name"] = properties.name.trim();
+  }
+  
+  if (properties.status && properties.status.trim() !== "") {
+    raw["status"] = properties.status.trim();
+  }
+  
+  if (properties.remoteJid && properties.remoteJid.trim() !== "") {
+    raw["remoteJid"] = properties.remoteJid.trim();
+  }
+  
+  
+
+  
+
+  // Converta o objeto raw em uma string JSON
+  var rawString = JSON.stringify(raw);
+
+  var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: rawString,
+     
+  };
+  
+
+  
+
+
+  
+  
+
+instance.publishState('resultado', '');
+instance.publishState('error', false);
+instance.publishState('error_log', '');
+
+
+fetch(url, requestOptions)
+  .then(response => {
+      if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+  })
+  .then(resultObj => {
+     
+             
+      if (Object.keys(resultObj).length > 0) {
+          instance.publishState('resultado', JSON.stringify(resultObj, null, 2).replace(/"_p_/g, "\""));
+    		console.log('resultado', JSON.stringify(resultObj, null, 2));
+          
+          instance.publishState('logs', resultObj);
+          
+          
+      }
+  })
+  .catch(error => {
+      instance.publishState('error', true);
+      try {
+      // Tentativa de converter a mensagem de erro para um objeto e formatá-lo como JSON
+      let errorString = error.toString().replace(/"_p_/g, "\"");
+      let errorObject = JSON.parse(errorString);
+      let formattedError = JSON.stringify(errorObject, null, 2);
+      instance.publishState('error_log', formattedError);
+         
+     } catch(e) {
+      // Se a conversão falhar, apenas use a mensagem de erro como uma string
+      let errorString = error.toString().replace(/"_p_/g, "\"");
+      instance.publishState('error_log', errorString);
+      }
+  });
+
+
+}
---
+++ elements/AAC-850m6/element_actions/ACD-851m8/run.js
@@ -0,0 +1,111 @@
+function(instance, properties, context) {
+    
+  let baseUrl = properties.url;
+  if (!baseUrl || baseUrl.trim() === "" || !baseUrl.includes("http")) {
+      baseUrl = context.keys["Server URL"];
+  }
+
+  if (baseUrl) {
+  baseUrl = baseUrl.trim();
+  }
+  if (baseUrl && baseUrl.endsWith("/")) {
+      baseUrl = baseUrl.slice(0, -1);
+  }
+
+
+  let instancia = properties.instancia;
+  if (!instancia || instancia.trim() === "") {
+      instancia = context.keys["Instancia"];
+  }
+
+  var url = baseUrl + "/automate/startFlow/" + instancia;
+  
+  
+  
+  var myHeaders = new Headers();
+  myHeaders.append("Accept", "*/*");
+  myHeaders.append("Connection", "keep-alive");
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("uazapi", "true");
+  myHeaders.append("apikey", properties.apikey);
+  
+
+  var raw = 
+  {
+    "name": properties.name,
+    "remoteJid": properties.remoteJid,
+
+   };
+  
+// Se properties.changeVariables existe e não é uma string vazia
+if (properties.changeVariables && properties.changeVariables.trim() !== "") {
+  try {
+    const parsedChangeVariables = JSON.parse(properties.changeVariables.trim());
+    raw["changeVariables"] = parsedChangeVariables;
+  } catch (e) {
+    console.error("Falha ao analisar changeVariables:", e);
+    // Não vai adicionar changeVariables ao raw em caso de erro
+  }
+}
+
+  
+
+  // Converta o objeto raw em uma string JSON
+  var rawString = JSON.stringify(raw);
+
+  var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: rawString,
+     
+  };
+  
+
+  
+
+
+  
+  
+
+instance.publishState('resultado', '');
+instance.publishState('error', false);
+instance.publishState('error_log', '');
+
+
+fetch(url, requestOptions)
+  .then(response => {
+      if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+  })
+ .then(resultObj => {
+     
+ 
+             
+      if (Object.keys(resultObj).length > 0) {
+          instance.publishState('resultado', JSON.stringify(resultObj, null, 2).replace(/"_p_/g, "\""));
+    		//console.log('resultado', JSON.stringify(resultObj, null, 2));
+          
+          //instance.publishState('log', resultObj);
+          
+      }
+  })
+  .catch(error => {
+      instance.publishState('error', true);
+      try {
+      // Tentativa de converter a mensagem de erro para um objeto e formatá-lo como JSON
+      let errorString = error.toString().replace(/"_p_/g, "\"");
+      let errorObject = JSON.parse(errorString);
+      let formattedError = JSON.stringify(errorObject, null, 2);
+      instance.publishState('error_log', formattedError);
+         
+     } catch(e) {
+      // Se a conversão falhar, apenas use a mensagem de erro como uma string
+      let errorString = error.toString().replace(/"_p_/g, "\"");
+      instance.publishState('error_log', errorString);
+      }
+  });
+
+
+}
