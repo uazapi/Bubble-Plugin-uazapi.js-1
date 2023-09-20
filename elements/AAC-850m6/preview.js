b03148495968aec98c7a557667dceb55c4318900
uazapi e afins <123725746+uazapi@users.noreply.github.com>
2023-09-20 13:43:19 -0300

    Automatic commit from Bubble

--- elements/AAC-850m6/preview.js
+++ elements/AAC-850m6/preview.js
@@ -1,5 +1,11 @@
 function(instance, properties) {
 
-
+	var preview = Math.min(properties.bubble.height(), properties.bubble.width());
+    var imgElement = document.createElement("IMG");
+    imgElement.setAttribute("src", "https://meta-l.cdn.bubble.io/f1687877424297x152106290190667800/uazap%20i%20%2815%29round.png");
+    imgElement.style.width = 0.8 * preview + "px";
+    imgElement.style.verticalAlign = "middle";
+    
+    instance.canvas[0].appendChild(imgElement);
 
 }
