a41717e03f200562a357a4688275a87a92ab6595
uazapi <123725746+uazapi@users.noreply.github.com>
2024-01-12 17:59:21 -0300

    ok, ajustar parametros primeiro

--- actions/AQJ-858mm/server.js
+++ actions/AQJ-858mm/server.js
@@ -1,8 +1,82 @@
 async function(properties, context) {
+    //▶️ Editar Config Geral XX
+    
+    let baseUrl = properties.url;
+    if (!baseUrl || baseUrl.trim() === "" || !baseUrl.includes("http")) {
+        baseUrl = context.keys["Server URL"];
+    }
 
+  
+    if (baseUrl) {
+    baseUrl = baseUrl.trim();
+    }
 
+    if (baseUrl && baseUrl.endsWith("/")) {
+    baseUrl = baseUrl.slice(0, -1);
+    }
+    
+    let apikey = properties.apikey;
+    if (!apikey || apikey.trim() === "") {
+        apikey = context.keys["Global APIKEY"];
+    }
+    
+    if (apikey) {
+    apikey = apikey.trim();
+    }
 
+    const url = `${baseUrl}/instance/create`;
+    
+    const headers = {
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        "Content-Type": "application/json",
+        "apikey": apikey
+    };
+    
+    const body = {
+        "instanceName": properties.instanceName,
+        "apikey": properties.apikeysenha,
+        
+    };
+    
+    if (properties.number) {
+    body.number = properties.number
+    }
+    
+    let response;
+    let error = false;
+    let error_log;
 
+    try {
+        response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body)
+        });
+    } catch(e) {
+        error = true;
+        error_log = e.toString();
+    }
 
+    if (!response.ok) {
+        error = true;
+        const responseBody = await response.json();
+        return {
+            error: error,
+            error_log: JSON.stringify(responseBody, null, 2).replace(/"_p_/g, "\"")
+        };
+    } 
 
+    const resultObj = await response.json();
+
+    return {
+        log: JSON.stringify(resultObj, null, 2).replace(/"_p_/g, "\""),
+        instancia: resultObj.instance?.instanceName,
+        status: resultObj.instance?.status,
+        apikey: resultObj.hash?.apikey,
+        qrcode: resultObj.qrcode?.base64,
+        paircode: resultObj.qrcode?.pairingCode,
+        error: error,
+        error_log: error_log        
+    };
 }
--- actions/AQO-858my/server.js
+++ actions/AQO-858my/server.js
@@ -1,8 +1,89 @@
 async function(properties, context) {
+    //▶️ Enviar localização XX
+    
+    let baseUrl = properties.url;
+    if (!baseUrl || baseUrl.trim() === "" || !baseUrl.includes("http")) {
+        baseUrl = context.keys["Server URL"];
+    }
 
+    if (baseUrl) {
+        baseUrl = baseUrl.trim();
+    }
+    
+    if (baseUrl && baseUrl.endsWith("/")) {
+        baseUrl = baseUrl.slice(0, -1);
+    }
 
+    let apikey = properties.apikey;
+    if (!apikey || apikey.trim() === "") {
+        apikey = context.keys["Global APIKEY"];
+    }
+    
+    if (apikey) {
+        apikey = apikey.trim();
+    }
 
+    let instancia = properties.instancia;
+    if (!instancia || instancia.trim() === "") {
+        instancia = context.keys["Instancia"];
+    }
 
+    const url = `${baseUrl}/message/sendLocation/${instancia}`;
+    
+    const headers = {
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        "Content-Type": "application/json",
+        "apikey": apikey
+    };
+    
+    const raw = {
+        "number": properties.number,
+        "locationMessage": {
+            "name": properties.name,
+            "address": properties.address,
+            "latitude": properties.latitude,
+            "longitude": properties.longitude
+        },
+        "options": {
+            "delay": properties.delay
+        }
+    };
 
+    let response;
+    let error = false;
+    let error_log;
 
+    try {
+        response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(raw)
+        });
+
+        if (!response.ok) {
+            error = true;
+            const responseBody = await response.json();
+            return {
+                error: error,
+                error_log: JSON.stringify(responseBody, null, 2).replace(/"_p_/g, "\"")
+            };
+        }
+
+    } catch (e) {
+        error = true;
+        error_log = e.toString();
+    }
+
+    const resultObj = await response.json();
+
+    return {
+        remoteJid: resultObj?.key?.remoteJid,
+        fromMe: resultObj?.key?.fromMe,
+        id: resultObj?.key?.id,
+        status: resultObj?.status,
+        error: error,
+        log: JSON.stringify(resultObj, null, 2).replace(/"_p_/g, "\""),
+        error_log: error_log
+    };
 }
